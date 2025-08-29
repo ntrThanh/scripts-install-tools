82d04dbd3a28a7a23db70efbef34d1dc2b1a7f4e
Trong Thanh <nguyentrongthanh672@gmail.com>
2025-08-29 10:23:01 +0700

    init commit

---
+++ installer.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+# Tools cơ bản
+sudo apt install -y git neovim htop gedit syncthing code cheese sqlite3
+
+# Python + Jupyter
+sudo apt install -y python3 python3-pip python3-venv jupyter-notebook
+
+# Java JDK 21
+sudo apt install -y openjdk-21-jdk
+
